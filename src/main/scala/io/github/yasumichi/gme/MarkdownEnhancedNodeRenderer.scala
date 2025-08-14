6359c9c9de486f49dc12f25ed8555e3e74f01c36
Yasumichi Akahoshi <sekiseijuju@gmail.com>
2025-08-14 14:35:37 +0900

    Format by scalafmt

--- src/main/scala/io/github/yasumichi/gme/MarkdownEnhancedNodeRenderer.scala
+++ src/main/scala/io/github/yasumichi/gme/MarkdownEnhancedNodeRenderer.scala
@@ -3,7 +3,12 @@ package io.github.yasumichi.gme
 import com.vladsch.flexmark.ast.FencedCodeBlock
 import com.vladsch.flexmark.html.HtmlRendererOptions
 import com.vladsch.flexmark.html.HtmlWriter
-import com.vladsch.flexmark.html.renderer.{NodeRenderer, NodeRendererContext, NodeRendererFactory, NodeRenderingHandler}
+import com.vladsch.flexmark.html.renderer.{
+  NodeRenderer,
+  NodeRendererContext,
+  NodeRendererFactory,
+  NodeRenderingHandler
+}
 import com.vladsch.flexmark.util.data.DataHolder
 import com.vladsch.flexmark.util.sequence.BasedSequence
 
@@ -18,38 +23,51 @@ import java.nio.charset.Charset
 
 class MarkdownEnhancedNodeRenderer extends NodeRenderer {
 
-  override def getNodeRenderingHandlers(): util.Set[NodeRenderingHandler[_ <: Object]] = {
-    val set: util.HashSet[NodeRenderingHandler[_]] = new util.HashSet[NodeRenderingHandler[_]]
-    set.add(new NodeRenderingHandler[FencedCodeBlock](classOf[FencedCodeBlock], this.render))
+  override def getNodeRenderingHandlers()
+      : util.Set[NodeRenderingHandler[_ <: Object]] = {
+    val set: util.HashSet[NodeRenderingHandler[_]] =
+      new util.HashSet[NodeRenderingHandler[_]]
+    set.add(
+      new NodeRenderingHandler[FencedCodeBlock](
+        classOf[FencedCodeBlock],
+        this.render
+      )
+    )
     set
   }
 
-  private def render(node: FencedCodeBlock, context: NodeRendererContext, html: HtmlWriter): Unit = {
-    val htmlOptions:HtmlRendererOptions  = context.getHtmlOptions()
-    val language:BasedSequence  = node.getInfoDelimitedByAny(htmlOptions.languageDelimiterSet)
+  private def render(
+      node: FencedCodeBlock,
+      context: NodeRendererContext,
+      html: HtmlWriter
+  ): Unit = {
+    val htmlOptions: HtmlRendererOptions = context.getHtmlOptions()
+    val language: BasedSequence =
+      node.getInfoDelimitedByAny(htmlOptions.languageDelimiterSet)
 
     if (language.equals("plantuml")) {
-        var text = ""
-        var seqs = node.getContentLines().toArray()
-        for(i <- 0 to seqs.length - 1) text = text + seqs(i).toString + "\n"
+      var text = ""
+      var seqs = node.getContentLines().toArray()
+      for (i <- 0 to seqs.length - 1) text = text + seqs(i).toString + "\n"
 
       val reader = new SourceStringReader(text)
       val os = new ByteArrayOutputStream()
       reader.generateImage(os, new FileFormatOption(FileFormat.SVG))
-      
+
       html.tag("div")
       html.append(new String(os.toByteArray(), Charset.forName("UTF-8")))
-      html.tag("/div") 
+      html.tag("/div")
       os.close()
 
     } else {
-        context.delegateRender()
+      context.delegateRender()
     }
   }
 }
 
 object MarkdownEnhancedNodeRenderer {
   class Factory() extends NodeRendererFactory {
-    override def apply(options: DataHolder): NodeRenderer = new MarkdownEnhancedNodeRenderer()
+    override def apply(options: DataHolder): NodeRenderer =
+      new MarkdownEnhancedNodeRenderer()
   }
 }
