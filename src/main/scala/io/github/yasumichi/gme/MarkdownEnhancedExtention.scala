53fa6cc6837d3c184df334365e14921519c4480e
Yasumichi Akahoshi <sekiseijuju@gmail.com>
2025-12-07 12:54:28 +0900

    add scaladoc comments.

--- src/main/scala/io/github/yasumichi/gme/MarkdownEnhancedExtention.scala
+++ src/main/scala/io/github/yasumichi/gme/MarkdownEnhancedExtention.scala
@@ -6,8 +6,16 @@ import com.vladsch.flexmark.parser.Parser
 import com.vladsch.flexmark.parser.Parser.ParserExtension
 import com.vladsch.flexmark.util.data.MutableDataHolder
 
+/**
+  * Markdown Enhanced Extension for flexmark-java
+  */
 class MarkdownEnhancedExtention extends ParserExtension with HtmlRendererExtension {
 
+  /**
+  * Extend the parser with custom inline parser extension
+  *
+  * @param parserBuilder The parser builder to extend
+  */
   override def extend(parserBuilder: Parser.Builder): Unit = {
     parserBuilder.customInlineParserExtensionFactory(new MarkInlineParserExtension.Factory())
   }
@@ -16,12 +24,23 @@ class MarkdownEnhancedExtention extends ParserExtension with HtmlRendererExtensi
 
   override def rendererOptions(options: MutableDataHolder): Unit = {}
 
+  /**
+  * Extend the HTML renderer with custom node renderer and link resolver
+  *
+  * @param htmlRendererBuilder The HTML renderer builder to extend
+  * @param rendererType The type of renderer being built (usually "HTML")
+  */
   override def extend(htmlRendererBuilder: HtmlRenderer.Builder, rendererType: String): Unit = {
     htmlRendererBuilder.nodeRendererFactory((new MarkdownEnhancedNodeRenderer.Factory()))
     htmlRendererBuilder.linkResolverFactory((new MarkdownEnhancedLinkResolver.Factory()))
   }
 }
 
+/**
+  * Companion object for MarkdownEnhancedExtention
+  * Provides a factory method to create an instance of the extension
+  * @return A new instance of MarkdownEnhancedExtention
+  */
 object MarkdownEnhancedExtention {
   def create(): MarkdownEnhancedExtention = new MarkdownEnhancedExtention()
 }
